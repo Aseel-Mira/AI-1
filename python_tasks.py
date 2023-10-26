1f4adae62956d96ea66bba164e86ca360151e773
Aseel-Mira <aseel.mira@hotmail.com>
2023-10-26 08:57:34 +0300

    Update python_tasks.py

--- python_tasks.py
+++ python_tasks.py
@@ -1,4 +1,10 @@
+# -*- coding: utf-8 -*-
 """python-tasks.ipynb
+Original file is located at
+    https://colab.research.google.com/gist/Aseel-Mira/6436b2e999f4f9bf31406368be3fa219/python-tasks.ipynb
+
+- A program that takes an amount from the user, asks him for the currency to be converted to, and the amount is converted
+"""
 x = input()
 
 print(x[0:2])
